8aade6ad15266ca60a3cd98f43b2dd9dc563ecc8
satio7283 <satio7283@gmail.com>
2021-08-11 19:52:05 +0200

    Create pattern_name.py

---
+++ task2/pattern_name.py
@@ -0,0 +1,189 @@
+m = 0
+while m <3:
+	print("*"*15) 
+	m+=1
+x =0 
+while x<6 :
+#	print(" "*9 )	
+	print("*****") 
+	x+=1
+z =0
+b=0
+while b <3:
+	print("*"*15)
+	b+=1
+while z<6 :
+#	print(" "*9 )	
+	print("           ****")
+	z+=1	
+c=0	
+while c <3:
+	print("*"*15)
+	c+=1
+
+d=0
+while d <3:
+    print("        ****************")
+    d+=1
+
+e=0
+while e <3:
+    print("       ******      ******")
+    e+=1
+    
+f=0
+while f <=8:
+    print("     ******           ******")
+    f+=1
+    
+    
+f=0
+while f <3:
+    print("     ****** ********* ******")
+    f+=1
+
+    
+f=0
+while f <=8:
+    print("     ******           ******")
+    f+=1
+
+
+d=0
+while d <3:
+    print("        ****************")
+    d+=1
+
+e=0
+while e <3:
+    print("       ******      ******")
+    e+=1
+    
+f=0
+while f <=8:
+    print("     ******           ******")
+    f+=1
+    
+    
+f=0
+while f <3:
+    print("     ****** ********* ******")
+    f+=1
+
+    
+f=0
+while f <=8:
+    print("     ******           ******")
+    f+=1
+# the last character (D)
+
+h=0
+while h <1:
+    print("*******  **** ")
+    h+=1
+
+
+h=0
+while h <1:
+    print("*******  ****** ")
+    h+=1
+
+
+h=0
+while h <1:
+    print("*******    ******** ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+    
+h=0
+while h <1:
+    print("*******     ********* ")
+    h+=1
+
+h=0
+while h <1:
+    print("*******    ******** ")
+    h+=1
+
+h=0
+while h <1:
+    print("*******  ****** ")
+    h+=1
+
+
+h=0
+while h <1:
+    print("*******  **** ")
+    h+=1
+
